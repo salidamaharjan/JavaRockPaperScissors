9f94e3ed730eee13341891796a6e10ff270b20ca
Salida Maharjan <salidamaharjan@outlook.com>
2024-11-25 11:35:35 -0600

    Check user response to continue or not

--- PlayRockPaperScissors.java
+++ PlayRockPaperScissors.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
+
 public class PlayRockPaperScissors {
     public static void checkStatus() {
         Scanner scnr = new Scanner(System.in);
 
         System.out.print("Enter ROCK or PAPER or SCISSORS: ");
         String userChoice = scnr.next().toUpperCase();
+        System.out.println(userChoice);
+        if ((!userChoice.equals("ROCK")) && (!userChoice.equals("PAPER")) && (!userChoice.equals("SCISSORS"))) {
+            System.out.print("Please enter ROCK or PAPER or SCISSORS: ");
+            userChoice = scnr.next().toUpperCase();
+        }
 
         String[] choices = {"ROCK", "PAPER", "SCISSORS"};
         int randomNum = (int) Math.floor(Math.random() * 3);
         String compChoice = choices[randomNum];
         System.out.println("Computer's Choice: " + compChoice);
 
-        while(userChoice.equals(compChoice)){
+        while (userChoice.equals(compChoice)) {
             System.out.print("It's a DRAW!! Enter ROCK or PAPER or SCISSORS: ");
             userChoice = scnr.next().toUpperCase();
+            if ((!userChoice.equals("ROCK")) && (!userChoice.equals("PAPER")) && (!userChoice.equals("SCISSORS"))) {
+                System.out.print("Please enter ROCK or PAPER or SCISSORS: ");
+                userChoice = scnr.next().toUpperCase();
+            }
             randomNum = (int) Math.floor(Math.random() * 3);
             compChoice = choices[randomNum];
-            System.out.print("Computer's Choice: " + compChoice);
+            System.out.println("Computer's Choice: " + compChoice);
         }
-        if((userChoice.equals("ROCK") && compChoice.equals("SCISSORS")) || (userChoice.equals("PAPER") && compChoice.equals("ROCK")) || (userChoice.equals("SCISSORS") && compChoice.equals("PAPER"))){
+
+        if ((userChoice.equals("ROCK") && compChoice.equals("SCISSORS")) || (userChoice.equals("PAPER") && compChoice.equals("ROCK")) || (userChoice.equals("SCISSORS") && compChoice.equals("PAPER"))) {
             System.out.println("YOU 🏆WIN!!");
         } else {
             System.out.println("YOU 🥶LOSE!!");
@@ -26,6 +37,23 @@ public class PlayRockPaperScissors {
     }
 
     public static void main(String[] args) {
-       checkStatus();
+        Scanner scnr = new Scanner(System.in);
+        checkStatus();
+        System.out.print("Type YES to play and NO to exit: ");
+        String playAgain = scnr.next().toUpperCase();
+        if(!playAgain.equals("YES") && !playAgain.equals("NO")){
+            System.out.print("Please type YES to play and NO to exit: ");
+            playAgain = scnr.next().toUpperCase();
+        }
+        while (playAgain.equals("YES")) {
+            checkStatus();
+            System.out.print("Type YES to play and NO to exit: ");
+            playAgain = scnr.next().toUpperCase();
+            if(!playAgain.equals("YES") && !playAgain.equals("NO")){
+                System.out.print("Please type YES to play and NO to exit: ");
+                playAgain = scnr.next().toUpperCase();
+            }
+        }
+
     }
 }
