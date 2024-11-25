702966fa1d4c0438f0c7c4d6d00ebae8ab1548f7
Salida Maharjan <salidamaharjan@outlook.com>
2024-11-25 10:41:30 -0600

    Print draw if the userChoice equals computer choice

--- PlayRockPaperScissors.java
+++ PlayRockPaperScissors.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 public class PlayRockPaperScissors {
-    public static void randomStatus(String userInput) {
-        String[] RPS = {"ROCK", "PAPER", "SCISSORS"};
+    public static void checkStatus() {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.print("Enter ROCK or PAPER or SCISSORS: ");
+        String userChoice = scnr.next().toUpperCase();
+
+        String[] choices = {"ROCK", "PAPER", "SCISSORS"};
         int randomNum = (int) Math.floor(Math.random() * 3);
-        String RPSStatus = RPS[randomNum];
-        System.out.print("Computer's Choice: " + RPSStatus);
+        String compChoice = choices[randomNum];
+        System.out.println("Computer's Choice: " + compChoice);
+
+        while(userChoice.equals(compChoice)){
+            System.out.print("It's a DRAW!! Enter ROCK or PAPER or SCISSORS: ");
+            userChoice = scnr.next().toUpperCase();
+            randomNum = (int) Math.floor(Math.random() * 3);
+            compChoice = choices[randomNum];
+            System.out.print("Computer's Choice: " + compChoice);
+        }
+        if(userChoice.equals("ROCK") && compChoice.equals("SCISSORS")){
+            System.out.println("You Win!!");
+        }
     }
 
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-        System.out.print("Enter Rock or Paper or Scissors: ");
-        String userInput = scnr.next().toUpperCase();
-        randomStatus(userInput);
+       checkStatus();
     }
 }
