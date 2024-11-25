0d69af02df7f3fee8a464e23e1c5b1e88312891d
Salida Maharjan <salidamaharjan@outlook.com>
2024-11-25 10:15:26 -0600

    Create array and random number

--- PlayRockPaperScissors.java
+++ PlayRockPaperScissors.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 public class PlayRockPaperScissors {
-    public static void checkStatus() {}
+    public static int checkStatus() {
+        String[] RPS = {"ROCK", "PAPER", "SCISSORS"};
+        int randomRPS = (int) Math.floor(Math.random() * 3);
+        return randomRPS;
+    }
+
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         System.out.print("Enter Rock or Paper or Scissors: ");
         String status = scnr.next();
+        System.out.println(checkStatus());
     }
 }
