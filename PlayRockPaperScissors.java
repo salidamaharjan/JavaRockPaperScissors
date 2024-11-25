518ef5db3042c94e196f1ab5c8015b929e8323e6
Salida Maharjan <salidamaharjan@outlook.com>
2024-11-25 10:04:15 -0600

    Create a project and prompt the user to enter status

---
+++ PlayRockPaperScissors.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class PlayRockPaperScissors {
+    public static void checkStatus() {}
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("Enter Rock or Paper or Scissors: ");
+        String status = scnr.next();
+    }
+}
